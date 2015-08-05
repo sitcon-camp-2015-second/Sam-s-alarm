bfc6048ee8f521204d965ab5f819307e74a04afc
Sean <Sean900604@gmail.com>
2015-08-05 21:51:31 +0800

    Add git ignore

--- js/1.js
+++ js/1.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-	//點擊新增，增加一個鬧鐘
-
   //產生選單
 for(i=0; i<24; i++) {
   $("#hours").append('<option value="' + i + '">' + i + '</select>');
 }
-
 for(i=0; i<60; i++) {
   $("#minutes").append('<option value="' + i + '">' + i + '</select>');
 }
-
-  //一個下拉bar可以設定鬧鐘
-
-	//勾選鬧鐘是否開啟
 });
+
+//開始鬧鐘
+function start_alarm() {
+  time = document.querySelector("#hours").value*60 + document.querySelector("#minutes").value
+  sleep(time*1000)
+  alert()
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
