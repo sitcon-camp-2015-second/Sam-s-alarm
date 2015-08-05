286f8b21ba38102c47e78abbc8cf46c7832c0c44
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2015-08-05 20:53:52 +0800

    add play and pause function

--- music/music_db_javascript.js
+++ music/music_db_javascript.js
@@ -7,7 +7,7 @@ function aCT()
 
 
 
-function aCheck()
+function aCheck()		//檢查播放狀態
 {
 	var player = document.getElementById('audio_c'),  
     map =  ['error','src','currentSrc','networkState','readyState','preload','buffered','played','seekable','seeking','currentTime','startTime','duration','paused','defaultPlaybackRate','playbackRate','ended','autoplay','loop','controls','volume','muted'];  
@@ -20,4 +20,17 @@ function aCheck()
 function aCK()
 {
 
+}
+
+
+function aplay()		//播放方法
+{
+	var audio = document.getElementsByTagName('audio')[0];
+	audio.play();
+}
+
+function apause()		//暫停
+{
+	var audio = document.getElementsByTagName('audio')[0];
+	audio.pause();
 }
